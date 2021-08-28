1c3fcae604289efeafbdf5185a63480378c22f22
Max <maxzheleznyy@gmail.com>
2021-08-28 14:53:08 +0300

    Solve 172. Factorial Trailing Zeroes
    Fix #83

---
+++ 172FactorialTrailingZeroes.swift
@@ -0,0 +1,15 @@
+/*
+Given an integer n, return the number of trailing zeroes in factorial of n
+
+Examples:
+Input: n = 3
+Output: 0
+Explanation: 3! = 6, no trailing zero.
+
+Input: n = 5
+Output: 1
+Explanation: 5! = 120, one trailing zero.
+
+Input: n = 0
+Output: 0
+*/
